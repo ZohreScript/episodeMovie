4117a85b00d8d5950be128caebe71f8f4faeffb6
zohreScript <z.omidi74@gmail.com>
2023-12-29 22:41:19 +0330

    initial-commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/RickMortyEpisodes">
-      <App />
-    </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode basename="/RickMortyEpisodes">
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
